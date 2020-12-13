74461ef1bd5a1835594a837ea922b31c7dd6efa6
sandrabeme <sandrabino2000@gmail.com>
2020-12-13 14:58:27 +0530

    adding changes

--- loginreg/routes/dashboard.js
+++ loginreg/routes/dashboard.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 // Display the dashboard page
-router.get("/", (req, res) => {
+router.get("/dashboard", (req, res) => {
   res.render("dashboard");
 });
 
